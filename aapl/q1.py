ba966381e3f8848ff48a6de9ec5fd3a6f0518d50
Weigang Geng <gengwg@gmail.com>
2018-06-11 21:56:22 -0700

    add questions

--- aapl/q1.py
+++ aapl/q1.py
@@ -1,3 +1,18 @@
+"""
+Given a folder of build files that might look like this:
+
+/Build-1.dmg
+/Build-2.dmg
+/Build-2.1.dmg
+/Build-2.1.1.dmg
+/Build-3.dmg
+/Build-3.2.dmg
+/Build-3.19.dmg
+
+Write a script that takes a path to the folder as an argument, and prints the highest build number (just the number, i.e. 2.1.1)
+Note:  The first digit can go up to 999 and should always be present, but the second and third can only go up to 99, and might not be         included.
+"""
+
 import re
 
 builds = '/Build-1.dmg /Build-2.dmg /Build-2.1.dmg /Build-2.1.1.dmg /Build-3.dmg /Build-3.2.dmg /Build-3.19.dmg'
@@ -21,7 +36,6 @@ def compareVersion(version1, version2):
     return 0
 
 def getHighestVersion(builds):
-
     highest = '0'
     pattern = re.compile(r'/Build-(.*).dmg')
     for build in builds.split():
--- aapl/q2.sh
+++ aapl/q2.sh
@@ -1,8 +1,5 @@
- $ ip -o addr | awk '{print $2, $4}'
- lo 127.0.0.1/8
- lo ::1/128
- wlp3s0 192.168.1.77/24
- wlp3s0 2600:1700:9770:1e90:c0e7:171e:985b:e92d/64
- wlp3s0 fe80::9240:f51a:f76d:3d4b/64
- virbr0 192.168.122.1/24
- docker0 172.17.0.1/16
+"""
+Write a script that outputs the network interfaces on a system with the corresponding IP addresses in CIDR format.
+Bonus points for including IPv6 addresses.
+"""
+$ ip -o addr | awk '{print $2, $4}'
